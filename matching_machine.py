1d06f5e8f1bca9b316b5133d687d2b34a8f24f64
Caique Andrade <cpinheroca@gmail.com>
2025-02-22 18:45:08 -0300

    Added a buy market order method

--- matching_machine.py
+++ matching_machine.py
@@ -1,7 +1,12 @@
+# TODO Make an unique trade
 # TODO transform the orders vec in an hashmap
+# TODO function to print the book
 # TODO Split the sell/buy vec in two differents arrays: limit and market
 # TODO implement the order verfication
 # TODO Verify the max id in get_buy_order
+# TODO implement the change order method
+
+# TODO Rewiew the str of book
 
 class Order:
     def __init__(self, str_order):
@@ -90,8 +95,6 @@ class OrderBook:
 
         return self.sell_side_vec.copy()
 
-
-
 class MatchingMachine:
     def __init__(self, book: OrderBook):
         self.book = book
@@ -109,7 +112,7 @@ class MatchingMachine:
             obj_order.id = self.book.sell_side_index
             self.book.sell_side_index += 1
 
-    def buy_limit_orders(self, buy_order: Order):
+    def buy_limit_order(self, buy_order: Order):
         for sell_order in self.book.get_sell_order():
             if (
                 # this order is executed if the ssell price is equal to or lower than the desired buy price
@@ -120,7 +123,7 @@ class MatchingMachine:
                 price = min(sell_order.price, buy_order.price)
                 print("Trade, price: {}, qty: {}".format(price, buy_order.qty))
 
-    def sell_limit_orders(self, sell_order: Order):
+    def sell_limit_order(self, sell_order: Order):
         for buy_order in self.book.get_buy_order():
             if (
                 # this order is executed if the buy price is equal to or greater than the desired sell price
@@ -131,6 +134,22 @@ class MatchingMachine:
                 price = max(buy_order.price, sell_order.price)
                 print("Trade, price: {}, qty: {}".format(price, sell_order.qty))
 
+    def buy_market_order(self, buy_order: Order):
+        # ordering the array based on price
+        lower_price = self.book.get_sell_order()
+        lower_price.sort(key = lambda order : order.price)
+        print(primary_book)
+        print(lower_price)
+
+        
+        for sell_order in lower_price:
+            if (
+                # this order is executed immediately at the best price found
+                sell_order.qty >= buy_order.qty
+            ):
+                # trade 
+                print("Trade, price: {}, qty: {}".format(sell_order.price, buy_order.qty))
+
 primary_book = OrderBook()
 machine = MatchingMachine(primary_book)
 
@@ -138,19 +157,19 @@ current_order = Order('limit buy 11 100')
 machine.add_order(current_order)
 # print(current_order)
 
-current_order = Order('limit sell 10 100')
+current_order = Order('limit sell 200 20')
 machine.add_order(current_order)
-# print(current_order)
 
+current_order = Order('limit sell 130 20')
+machine.add_order(current_order)
 
-# current_order = Order('limit sell 20 100')
-# machine.add_order(current_order)
-# # print(current_order)
+current_order = Order('limit sell 160 20')
+machine.add_order(current_order)
 
-# current_order = Order('limit sell 20 200')
-# machine.add_order(current_order)
+current_order = Order('limit sell 195 20')
+machine.add_order(current_order)
 
-machine.sell_limit_orders(current_order)
+machine.buy_market_order(current_order)
 # print(current_order)
 
 # print(primary_book.get_buy_order())
